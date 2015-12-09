1222bee066136f87e0592b0ef39166e6e2170904
Si <siwang22@gmail.com>
2015-12-09 14:41:23 -0800

    (add) Working on documentation for server signup

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,7 +1,11 @@
 var userController = require('./userController.js');
 
-
+/** @userRoutes */
 module.exports = function (app) {
-  // app === userRouter from server
+  /**
+   * userRoutes function
+   * routes to controller according to url
+   * @param app - the userRouter (express.Router()) from the server
+   */
   app.post('/signup', userController.signup);
 };
