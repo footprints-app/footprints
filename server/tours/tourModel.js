5099f6a0ffb3e6138e19da3e29c1ae756e7ea6d6
Si <siwang22@gmail.com>
2015-12-10 20:22:07 -0800

    (feat) Wrote create tour controller and model functions

--- server/tours/tourModel.js
+++ server/tours/tourModel.js
@@ -32,5 +32,51 @@ module.exports = {
 				callback(err, results);
 			}
 		});
+	},
+  /**
+   * Inserts or selects a city from the city table.
+	 * Queries the cities table for a city. 
+	 * If it exists, the city's data is given to the callback.
+	 * If it doesn't exist, the city is added to the table and the data is given to the callback.
+	 *
+   * @param {string} params - an array containing the cityName, state, and country
+   * @param {function} callback - a callback which will take the arguments err and results from the database query
+   */
+	addOrGetCity: function(params, callback) {
+		var selectQuery = "SELECT * from cities WHERE cityName = (" + params[0] + ") \
+		                   AND state = (" + params[1] + ") AND country = (" + params[2] + ")";
+		var insertQuery = "INSERT into cities(cityName, state, country) \
+                       value (?, ?, ?, ?)";
+		db.query(selectQuery, params, function(err, results) {
+			if(err) {
+				console.log("did not get past city query");
+				callback(err);
+			} else {
+				if(results.length !== 0) {
+					console.log("results from city query: ", results);
+					callback(err, results[0]);
+				} else {
+					db.query(insertQuery, params, function(err, results) {
+						if(err) {
+							callback(err);
+						} else {
+							callback(err, results[0]);
+						}
+					})
+				}
+			}
+		})
+	},
+
+	insertTour: function(params, callback) {
+		var queryStr = "INSERT into tours(tourName, userId, description, category, duration, cityId) \
+			              value (?, ?, ?, ?, ?, ?)";
+		db.query(queryStr, function(err, results) {
+			if(err) {
+				callback(err);
+			} else {
+				callback(err, results[0]);
+			}
+		})
 	}
 };
