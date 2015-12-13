1d87a71bbd3e5b951123680830e7cf0da14f8002
Shashi <shashi@Shashis-MacBook-Pro.local>
2015-12-12 16:49:31 -0800

    compiling stylesheet

--- mobile/components/Login.js
+++ mobile/components/Login.js
@@ -45,15 +45,15 @@ class Login extends Component {
     utils.makeRequest('login', reqBody)
     .then((response) => {
       // console.log('response body: ', response);
-      if(response.error) {
-        if(response.error === 'Username does not exist') {
-          this.setState({validUsername: false, username: '', password: ''});
-        } else if(response.error === 'Username and password do not match') {
-          this.setState({validPassword: false, username: '', password: ''});
+      if( response.error ) {
+        if( response.error === 'Username does not exist' ) {
+          this.setState({ validUsername: false, username: '', password: '' });
+        } else if( response.error === 'Username and password do not match' ) {
+          this.setState({ validPassword: false, username: '', password: '' });
         }
       } else {
         var user = response;
-        utils.navigateTo.call(this, "Welcome", Main, {user} );
+        utils.navigateTo.call(this, "Welcome", Main, {user});
       }
     })
     .catch((error) => {
@@ -63,45 +63,53 @@ class Login extends Component {
 
   render () {
     return (
-      <View style={styles.container}>
-        <Image style={styles.bg} source={{uri: 'http://i.imgur.com/xlQ56UK.jpg'}} />
+      <View style={ styles.container }>
+        <Image style={ styles.bg } source={{ uri: 'http://i.imgur.com/xlQ56UK.jpg' }} />
         
-        <View style={styles.loginInputs}>
-          <View style={styles.inputContainer}>
-            <Image style={styles.inputUsername} source={{uri: 'http://i.imgur.com/iVVVMRX.png'}}/>
+        <View style={ styles.loginInputs }>
+          <View style={ styles.inputContainer }>
+            <Image style={ styles.inputUsername } source={{ uri: 'http://i.imgur.com/iVVVMRX.png' }}/>
             <TextInput 
-              style={[styles.input, styles.whiteFont]}
+              style={ [styles.input, styles.whiteFont] }
               placeholder="Username"
               placeholderTextColor="#FFF"
-              value={this.state.username}
-              onChange={utils.usernameInput.bind(this)}/>
+              value={ this.state.username }
+              onChange={ utils.usernameInput.bind(this) }/>
           </View>
-          <View style={styles.inputContainer}>
-            <Image style={styles.inputPassword} source={{uri: 'http://i.imgur.com/ON58SIG.png'}}/>
+          <View style={ styles.inputContainer }>
+            <Image style={ styles.inputPassword } source={{ uri: 'http://i.imgur.com/ON58SIG.png' }}/>
             <TextInput
               password={true}
-              style={[styles.input, styles.whiteFont]}
+              style={ [styles.input, styles.whiteFont] }
               placeholder="Password"
               placeholderTextColor="#FFF"
-              value={this.state.password}
-              onChange={utils.passwordInput.bind(this)}/>
+              value={ this.state.password }
+              onChange={ utils.passwordInput.bind(this) }/>
           </View>
 
-          <Text style={styles.whiteFont}> {this.state.validUsername ? '' : 'Sorry this username does not exist, please try again'} </Text>
-          <Text style={styles.whiteFont}> {this.state.validPassword ? '' : 'Sorry this username and password do not match, please try again'} </Text>
+          <Text style={ styles.whiteFont }>
+            { this.state.validUsername ? '' : 'Sorry this username does not exist, please try again' } 
+          </Text>
+          <Text style={ styles.whiteFont }>
+            { this.state.validPassword ? '' : 'Sorry this username and password do not match, please try again' }
+          </Text>
 
         </View>
 
-        <TouchableHighlight onPress={ this.submitLogin.bind(this) } style={styles.touchable} underlayColor="#FF3366">  
-          <View style={styles.signin}>
-            <Text style={styles.whiteFont}>Login</Text>
+        <TouchableHighlight 
+          onPress={ this.submitLogin.bind(this) } 
+          style={ styles.touchable } 
+          underlayColor="#FF3366">  
+          <View style={ styles.signin }>
+            <Text style={ styles.whiteFont }>Login</Text>
           </View>
         </TouchableHighlight>
 
-        <View style={styles.loginSignup}>
-          <Text style={styles.greyFont}>Do not have an account?</Text>
-          <TouchableHighlight onPress={ utils.navigateTo.bind(this, 'Signup', require('./Signup'), {}) }>  
-            <Text style={styles.whiteFont}>Sign Up</Text>
+        <View style={ styles.loginSignup }>
+          <Text style={ styles.greyFont }>Do not have an account?</Text>
+          <TouchableHighlight 
+            onPress={ utils.navigateTo.bind(this, 'Signup', require('./Signup'), {}) }>  
+            <Text style={ styles.whiteFont }>Sign Up</Text>
           </TouchableHighlight>
         </View>
       </View>
--- mobile/lib/stylesheet.js
+++ mobile/lib/stylesheet.js
@@ -15,7 +15,6 @@ var {
 } = React;
 
 var styles = StyleSheet.create({
-
   //Start Shared Styles login-signup
   container: {
     flexDirection: 'column',
@@ -70,8 +69,7 @@ var styles = StyleSheet.create({
   },
   //End Shared Styles
 
-  //Login.js Styles
-  
+  //Login.js Styles  
   loginSignup: {
     justifyContent: 'center',
     alignItems: 'center',
@@ -84,13 +82,12 @@ var styles = StyleSheet.create({
   },
   //End of Login.js Styles
 
-  //Start SignUp.js
-  
+  //Start SignUp.js  
   signup: {
-      backgroundColor: '#FF3366',
-      padding: 20,
-      alignItems: 'center',
-      marginTop: -8.5
+    backgroundColor: '#FF3366',
+    padding: 20,
+    alignItems: 'center',
+    marginTop: -8.5
   },
   login: {
     justifyContent: 'center',
@@ -98,12 +95,41 @@ var styles = StyleSheet.create({
     flex: .15
   },
   signupInputs: {
-      marginTop: 200,
-      marginBottom: 10,
-      flex: .25
+    marginTop: 200,
+    marginBottom: 10,
+    flex: .25
+  },  
+  //End SignUp.js
+
+  //Start Main.js
+  mainContainer: {
+    marginTop: 65,
+    padding: 10
   },
-  
-  //SignUp.js
+  mainButton: {
+    height: 200,
+    width: 200,
+    backgroundColor: '#FFF366',
+    borderRadius: 100,
+    justifyContent: 'center',
+    marginLeft: 75
+  },
+  mainButtonTop: {
+    marginTop: 60,
+  },
+  mainButtonBottom: {
+    marginTop: 15,
+  },
+  mainButtonText: {
+    fontSize: 24,
+    color: 'gray',
+    alignSelf: 'center'
+  },
+  mainTouchable: {
+    borderRadius: 100
+  }
+  //End Main.js
+
 })
 
 module.exports = styles;
