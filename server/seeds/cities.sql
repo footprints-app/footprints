82b511a86035c3e794bb8ee46ef4ede370ba1234
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2015-12-12 16:32:53 -0800

    (test) Added seeding files for DB

---
+++ server/seeds/cities.sql
@@ -0,0 +1,3 @@
+insert into cities (id, cityName, state, country) values (1, 'San Francisco', 'CA', 'United States');
+insert into cities (id, cityName, state, country) values (2, 'Alexandria', 'VA', 'United States');
+insert into cities (id, cityName, state, country) values (3, 'Rochester', 'NY', 'United States');
---
+++ server/seeds/users.sql
@@ -0,0 +1,4 @@
+insert into users (id, userName, firstName, lastName, password) values (1, 'nanderson0', 'Nicholas', 'Anderson', 'Maroon');
+insert into users (id, userName, firstName, lastName, password) values (2, 'acarroll1', 'Anne', 'Carroll', 'Purple');
+insert into users (id, userName, firstName, lastName, password) values (3, 'dnichols2', 'Deborah', 'Nichols', 'Khaki');
+insert into users (id, userName, firstName, lastName, password) values (4, 'ewallace3', 'Earl', 'Wallace', 'Indigo');
