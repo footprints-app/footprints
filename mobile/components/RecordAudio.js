e0df8b6e033c5e2aafbe902e4fc97cafdb588f0c
Si <siwang22@gmail.com>
2015-12-20 14:36:43 -0800

    (feat) Add file read function

--- mobile/components/RecordAudio.js
+++ mobile/components/RecordAudio.js
@@ -28,6 +28,32 @@ class RecordAudio extends Component {
       //cassetteGif: 'http://www.clicktorelease.com/code/gif/1.gif'
     }
   }
+
+  readDirectory() {
+    // get a list of files and directories in the main bundle
+    RNFS.readDir(RNFS.MainBundlePath)
+      .then((result) => {
+        console.log('GOT RESULT', result);
+
+        // stat the first file
+        //return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+      })
+      // .then((statResult) => {
+      //   if (statResult[0].isFile()) {
+      //     // if we have a file, read it
+      //     return RNFS.readFile(statResult[1], 'utf8');
+      //   }
+
+      //   return 'no file';
+      // })
+      // .then((contents) => {
+      //   // log the file contents
+      //   console.log(contents);
+      // })
+      // .catch((err) => {
+      //   console.log(err.message, err.code);
+      // });
+  }
   
   record() {
     this.setState({cassetteGif: require('../assets/cassette.gif')});
