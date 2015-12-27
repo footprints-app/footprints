f65be15492e4fe3917d12912509150517d6e1e9f
Rochelle Lee <rochelle.y.lee@gmail.com>
2015-12-27 14:01:11 -0800

    (refactor) removed checkAuth rout

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -8,5 +8,4 @@ var userController = require('./userController.js');
 module.exports = function (app) {
   app.post('/signup', userController.signup);
   app.post('/login', userController.login);
-  app.get('/auth', userController.checkAuth);
 };
