c25bd843f71c9a7971e31faa1e23d804de3418d0
Shashi <shashi@Shashis-MacBook-Pro.local>
2015-12-28 15:45:39 -0800

    create tour

--- mobile/components/CreateTour.js
+++ mobile/components/CreateTour.js
@@ -240,55 +240,6 @@ class CreateTour extends Component {
   }
 };
 
-// var styles = StyleSheet.create({
-//   container: {
-//     flexDirection: 'column',
-//     flex: 1,
-//     backgroundColor: 'transparent'
-//   },
-//   bg: {
-//     position: 'absolute',
-//     left: 0,
-//     top: 0,
-//     width: windowSize.width,
-//     height: windowSize.height
-//   },
-//   createTour: {
-//     backgroundColor: '#FF3366',
-//     padding: 20,
-//     alignItems: 'center',
-//     marginTop: -200,
-//   },
-//   inputs: {
-//     marginTop: 100,
-//     marginBottom: 10,
-//     flex: .25
-//   },
-//   inputContainer: {
-//     padding: 10,
-//     borderWidth: 1,
-//     borderBottomColor: '#CCC',
-//     borderColor: 'transparent'
-//   },
-//   input: {
-//     position: 'absolute',
-//     left: 10,
-//     top: 4,
-//     right: 0,
-//     height: 20,
-//     fontSize: 14
-//   },
-//   greyFont: {
-//     color: '#D8D8D8'
-//   },
-//   whiteFont: {
-//     color: '#FFF'
-//   },
-//   touchable: {
-//     borderRadius: 5
-//   }
-// });
-
 module.exports = CreateTour;
 
 
