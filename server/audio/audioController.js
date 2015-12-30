21cc708beade295044388f09ca8137030741bce8
Si <siwang22@gmail.com>
2015-12-29 16:17:14 -0800

    (fix) Add random string generator so that each audio upload creates a new key into the S3 database

--- server/audio/audioController.js
+++ server/audio/audioController.js
@@ -5,18 +5,27 @@
  */
 
 var aws = require('aws-sdk');
+var config = require('../../config/config.js');
 
-var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY_ID_AUDIO || 'AKIAJZBWCDR2624XAFLA';
-var AWS_SECRET_KEY = process.env.AWS_SECRET_ACCESS_KEY_AUDIO || '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';
-var S3_BUCKET = process.env.S3_BUCKET || 'walking-tour-media';
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY_ID_AUDIO || config.AWS_ACCESS_KEY_ID_AUDIO;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_ACCESS_KEY_AUDIO || config.AWS_SECRET_ACCESS_KEY_AUDIO;
+var S3_BUCKET = process.env.S3_BUCKET || config.S3_BUCKET;
 
 module.exports = {
+
+  randomString: function(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;
+  },
+
   signedUrl: function(req, res) {
     aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
     var s3 = new aws.S3();
+    var randomStr = this.randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
     var s3_params = {
         Bucket: S3_BUCKET,
-        Key: req.query.file_name || 'testFileName',//Change to the name of the file
+        Key: randomStr,
         Expires: 1000,
         //TODO: Error from S3 when ContentType is set.  What is the content-type for base64 encoded files?
         //ContentType: req.query.file_type || 'application/octet-stream',//Change to file type
@@ -27,11 +36,6 @@ module.exports = {
             console.log(err);
         }
         else{
-            // var return_data = {
-            //     signed_request: data,
-            //     url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
-            // };
-            // res.write(JSON.stringify(return_data));
             res.status(200).json({
               signed_request: data,
               url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+s3_params.Key
