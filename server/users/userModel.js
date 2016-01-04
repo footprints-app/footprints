8bbcabb4b40598d105f9182c5685e92e16b90c42
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2016-01-04 12:39:03 -0800

    (cleanup) Deleted unnecessary function and completed doc

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -66,7 +66,7 @@ module.exports = {
 
   /**
    * Queries database for user information given a specific userName
-   *
+	 *
    * @param {string} params - a userName
    * @param {function} callback - a callback which will take the arguments err and results from the database query
    */
@@ -82,6 +82,16 @@ module.exports = {
     })
   },
 
+	/**
+	 * Queries database for a userName.
+	 * If the userName does not exist in the database, the callback will take a defined error.
+	 * If the userName does exist then the password will be checked against the inputted password.
+	 * If the password does not match, the callback will take a defined error.
+	 * If the password does match, the callback will take an undefined error and the results of the query.
+	 *
+	 * @param {string} params - a tuple containing the userName and password
+	 * @param {function} callback - a callback which will take the arguments err and results from the database query
+	 */
   comparePassword: function (params, callback) { 
     var passwordQuery = "select * from users where userName = ?";
 
@@ -98,34 +108,6 @@ module.exports = {
           }
         }
     });
-  },
-
-  /**
-   * Queries database for a userName.
-   * If the userName does not exist in the database, the callback will take a defined error.
-   * If the userName does exist then the password will be checked against the inputted password.
-   * If the password does not match, the callback will take a defined error.
-   * If the password does match, the callback will take an undefined error and the results of the query.
-   *
-   * @param {string} params - a tuple containing the userName and password
-   * @param {function} callback - a callback which will take the arguments err and results from the database query
-   */
-
-  checkUserPassword: function(params, callback) {
-    var queryStr = "select * from users where userName = ?";
-
-    db.query(queryStr, params[0], function(err, results) {
-      if(results.length === 0) {
-        callback("Username does not exist");
-      } else {
-        if(results[0].password !== params[1]) {
-          callback("Username and password do not match");
-        } else {
-          results[0].password = ""; //Do not send the password back to the client
-          callback(err, results[0]);          
-        }
-      }
-    });
-  },
+  }
 
 };
